720e0ed6106ff806f93fc3b38827ecbad4cf43c5
Matthew Muriithi <muriithimatthew@gmail.com>
2023-05-02 23:51:45 -0400

    adding tests

---
+++ tests/test_app.py
@@ -0,0 +1,14 @@
+import unittest
+from ..app import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def testAddition(self):
+        a = 5
+        b = 4
+        expetected_result = 9
+        self.assertEqual(Calculator.add(a,b), expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
